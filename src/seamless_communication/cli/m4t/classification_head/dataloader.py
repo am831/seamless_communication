fcc47645f21200c48c93075c4e972b41badbe03f
Alisamar Husain <zrthxn@gmail.com>
2024-05-11 00:06:08 +0200

    src_lengths

--- src/seamless_communication/cli/m4t/classification_head/dataloader.py
+++ src/seamless_communication/cli/m4t/classification_head/dataloader.py
@@ -204,6 +204,7 @@ class UnitYLanguageIDDataLoader:
         
         while src_tokens.size(0) != self.batching_config.batch_size:
             src_tokens = torch.cat((src_tokens, src_tokens[-1].unsqueeze(0)), dim=0)
+            src_lengths = torch.cat((src_lengths, src_lengths[-1].unsqueeze(0)), dim=0)
             onehot_labels = torch.cat((onehot_labels, onehot_labels[-1].unsqueeze(0)), dim=0)
             
         return SeqsBatch(src_tokens=src_tokens, src_lengths=src_lengths), onehot_labels
